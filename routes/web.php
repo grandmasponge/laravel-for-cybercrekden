59b51d380cf378991de74d602f75199cfa157a67
grandmasponge <73469941+grandmasponge@users.noreply.github.com>
2023-01-02 14:34:02 +0000

    tehe

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\normalcontroller;
-use app\Http\Controllers\authcontroller;
+use App\Http\Controllers\NormalController;
+use App\Http\Controllers\AuthController;
 
 
 
-Route::get('/', [normalcontroller::class, 'index']);
-Route::get('/login', [authcontroller::class, 'login']);
-Route::get('/register', [authcontroller::class, 'register']);
+Route::get('/', [NormalController::class, 'index']);
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'register']);
